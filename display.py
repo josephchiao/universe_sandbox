71c32edf4cb8e39ed4e4a12d183d9593e4cdcfd2
josephchiao <chiaofeng.0118@gmail.com>
2024-10-20 10:16:41 -0700

    Added the data storage function.

--- display.py
+++ display.py
@@ -1,20 +1,36 @@
 import engine
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd 
 
-run_1 = engine.Engine()
-run_1.initialize()
-time_record, cord_record, v_record, acc_record = run_1.main()
 
+def run_engine():
+
+    run_1 = engine.Engine()
+    run_1.initialize()
+    time_record, cord_record, v_record, acc_record = run_1.main()
+    return time_record, cord_record, v_record, acc_record, run_1.names, run_1.duration, run_1.datapoints
+
+## Storage functions
+
+def data_storage(file):
+    time_record, cord_record, v_record, acc_record, names, duration, datapoints = run_engine()
+    np.savez(file, time_record = time_record, cord_record = cord_record, v_record = v_record, acc_record = acc_record, names = names, duration = duration, datapoints = datapoints)
+
+def data_retrieval(file):
+    
+    data = np.load(file)
+
+    return data['time_record'], data['cord_record'], data['v_record'], data['acc_record'], data['names'], data['duration'], data['datapoints']
 
 
 ## Display functions
 
-def static_displays():
+def static_displays(time_record, cord_record, v_record, acc_record, names):
 
     name_count = 0
     for record in cord_record:
-        plt.plot(time_record[0:len(record)], [record[i][0] for i in range(len(record))], label = run_1.names[name_count])            
+        plt.plot(time_record[0:len(record)], [record[i][0] for i in range(len(record))], label = names[name_count])            
         name_count += 1
     plt.title("x")
     plt.legend()
@@ -22,7 +38,7 @@ def static_displays():
 
     name_count = 0
     for record in cord_record:
-        plt.plot(time_record[0:len(record)], [record[i][1] for i in range(len(record))], label = run_1.names[name_count])
+        plt.plot(time_record[0:len(record)], [record[i][1] for i in range(len(record))], label = names[name_count])
         name_count += 1
     plt.title("y")
     plt.legend()
@@ -30,7 +46,7 @@ def static_displays():
 
     name_count = 0
     for record in cord_record:
-        plt.plot(time_record[0:len(record)], [record[i][2] for i in range(len(record))], label = run_1.names[name_count])
+        plt.plot(time_record[0:len(record)], [record[i][2] for i in range(len(record))], label = names[name_count])
         name_count += 1
     plt.title("z")
     plt.legend()
@@ -38,7 +54,7 @@ def static_displays():
 
     name_count = 0
     for record in v_record:
-        plt.plot(time_record[0:len(record)], [record[i][0] for i in range(len(record))], label = run_1.names[name_count])
+        plt.plot(time_record[0:len(record)], [record[i][0] for i in range(len(record))], label = names[name_count])
         name_count += 1
     plt.title("vx")
     plt.legend()
@@ -46,7 +62,7 @@ def static_displays():
 
     name_count = 0
     for record in v_record:
-        plt.plot(time_record[0:len(record)], [record[i][1] for i in range(len(record))], label = run_1.names[name_count])
+        plt.plot(time_record[0:len(record)], [record[i][1] for i in range(len(record))], label = names[name_count])
         name_count += 1
     plt.title("vy")
     plt.legend()
@@ -54,7 +70,7 @@ def static_displays():
 
     name_count = 0
     for record in v_record:
-        plt.plot(time_record[0:len(record)], [record[i][2] for i in range(len(record))], label = run_1.names[name_count])
+        plt.plot(time_record[0:len(record)], [record[i][2] for i in range(len(record))], label = names[name_count])
         name_count += 1
     plt.title("vz")
     plt.legend()
@@ -62,7 +78,7 @@ def static_displays():
     
     name_count = 0
     for record in acc_record:
-        plt.plot(time_record[0:len(record)], [record[i][0] for i in range(len(record))], label = run_1.names[name_count])
+        plt.plot(time_record[0:len(record)], [record[i][0] for i in range(len(record))], label = names[name_count])
         name_count += 1
     plt.title("ax")
     plt.legend()
@@ -70,7 +86,7 @@ def static_displays():
 
     name_count = 0
     for record in acc_record:
-        plt.plot(time_record[0:len(record)], [record[i][1] for i in range(len(record))], label = run_1.names[name_count])
+        plt.plot(time_record[0:len(record)], [record[i][1] for i in range(len(record))], label = names[name_count])
         name_count += 1
     plt.title("ay")
     plt.legend()
@@ -78,54 +94,65 @@ def static_displays():
 
     name_count = 0
     for record in acc_record:
-        plt.plot(time_record[0:len(record)], [record[i][2] for i in range(len(record))], label = run_1.names[name_count])
+        plt.plot(time_record[0:len(record)], [record[i][2] for i in range(len(record))], label = names[name_count])
         name_count += 1
     plt.title("az")
     plt.legend()
     plt.show()
         
-def active_display_2d():
+def active_display_2d(cord_record, names, duration, datapoints, leg = 300):
 
-    n = 0
+    count = len(cord_record[0])
+    replay = True
 
-    while True:
-        name_count = 0
-        for record in cord_record:
-            if n <= len(record):
-                plt.plot([record[i][0] for i in range(n)], [record[i][1] for i in range(n)])
-                plt.scatter([record[n-1][0]], [record[n-1][1]], label = run_1.names[name_count])
-                plt.legend()
-                name_count += 1
+    while replay:
+        for n in range(count):
+            name_count = 0
+            plt.clf()
+            for record in cord_record:
+                if n <= len(record):
+                    plt.plot([record[i][0] for i in range(max(0, n-leg), n)], [record[i][1] for i in range(max(0, n-leg), n)])
+                    plt.scatter([record[n-1][0]], [record[n-1][1]], label = names[name_count], s = 10)
+                    plt.legend()
+                    name_count += 1
 
-        plt.gca().set_aspect('equal')
-        plt.title(f"Time = {n*run_1.duration/run_1.datapoints/86400:.2f}days")
-        plt.pause(0.04)
-        plt.clf()
-        n += 1
-        if n > len(cord_record[0]):
-            n = 0
+            plt.gca().set_aspect('equal')
+            plt.title(f"Time = {n*duration/datapoints/86400:.2f}days")
+            plt.pause(0.04)
+        replay = input("Replay?") == ''
 
-def active_display_3d():
-    fig = plt.figure()
+
+
+def active_display_3d(cord_record, names, duration, datapoints, leg = 300):
     ax = plt.axes(projection='3d')
     n = 0
+    count = len(cord_record[0])
     plt.ion()
+    replay = True
 
-    while True:
-        name_count = 0
-        ax.clear()
-        for record in cord_record:
-            if n <= len(record):
-                ax.plot3D([record[i][0] for i in range(n)], [record[i][1] for i in range(n)], [record[i][2] for i in range(n)])
-                ax.scatter3D([record[n-1][0]], [record[n-1][1]], [record[n-1][2]], label = run_1.names[name_count])
-                ax.legend()
-                name_count += 1
+    while replay:
+        for n in range(count):
+            name_count = 0
+            ax.clear()
+            ax.set_box_aspect(np.ptp(np.array([record[i][0] for i in range(max(0, n-leg), n)])), np.ptp(np.array([record[i][1] for i in range(max(0, n-leg), n)])), np.ptp([record[i][2] for i in range(max(0, n-leg), n)]))
+            for record in cord_record:
+                if n <= len(record):
+                    ax.plot3D([record[i][0] for i in range(max(0, n-leg), n)], [record[i][1] for i in range(max(0, n-leg), n)], [record[i][2] for i in range(max(0, n-leg), n)])
+                    ax.scatter3D([record[n-1][0]], [record[n-1][1]], [record[n-1][2]], label = names[name_count])
+                    ax.legend()
+                    name_count += 1
 
-        ax.set_title(f"Time = {n*run_1.duration/run_1.datapoints/86400:.2f}days")
-        plt.pause(0.04)
-        n += 1
-        if n > len(cord_record[0]):
-            n = 0
+            ax.set_title(f"Time = {n*duration/datapoints/86400:.2f}days")
+            plt.pause(0.04)
+        replay = input("Replay?") == ''
+
 # static_displays()
 # active_display_2d()
-active_display_3d()
+# active_display_3d()
+
+# data_storage("saved_data/solar_system_over_1_year.npz")
+time_record, cord_record, v_record, acc_record, names, duration, datapoints = data_retrieval("saved_data/solar_system_over_1_year.npz")
+
+# active_display_2d(cord_record, names, duration, datapoints)
+# static_displays(time_record, cord_record, v_record, acc_record, names)
+active_display_3d(cord_record, names, duration, datapoints)
