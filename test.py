8fb0a4e97bfb26c724600ff11f1a7836fd598fe9
josephchiao <chiaofeng.0118@gmail.com>
2024-10-26 08:17:46 -0700

    -

--- test.py
+++ test.py
@@ -1,8 +1,27 @@
 ## Energy for blowing up planets calculator
-m = 102.409E+24
-r = 24624000
+# m = 2.29E+19
+# r = 222000
 
 
-E = (3*6.67e-11*m**2)/(5*r)
+# E = (3*6.67e-11*m**2)/(5*r)
 
-print(E)
+# print(E)
+import numpy as np
+
+
+data = np.load('saved_data/psyche_position_10years.npz')
+
+cord, velocity = data['cord_record'], data['v_record']
+
+cord_x = [c[-1][0] for c in cord]
+cord_y = [c[-1][1] for c in cord]
+cord_z = [c[-1][2] for c in cord]
+v_x = [v[-1][0] for v in velocity]
+v_y = [v[-1][1] for v in velocity]
+v_z = [v[-1][2] for v in velocity]
+print(cord_x)
+print(cord_y)
+print(cord_z)
+print(v_x)
+print(v_y)
+print(v_z)
